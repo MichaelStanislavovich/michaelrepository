fb88cf7013b8e8e57ce51136d706e7cae7650047
MichaelStanislavovich <143657967+MichaelStanislavovich@users.noreply.github.com>
2023-09-25 21:07:36 +0300

    Add files via upload

--- project/app_lesson_4/prilozh_app_lesson_4/models.py
+++ project/app_lesson_4/prilozh_app_lesson_4/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Advertisment(models.Model):
+    title = models.CharField(verbose_name="Заголовок", max_length=128)
+    description = models.TextField('Описание')
+    price=models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text="отметьте, если торг уместен")
+    created_at =models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Advertisment(id={self.id},title={self.title},price={self.price})'
+    class Meta:
+        db_table = 'advertisements'
