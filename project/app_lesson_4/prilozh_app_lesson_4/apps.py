e926640267ae9771424a5240e2beafb9e3c2f412
MichaelStanislavovich <143657967+MichaelStanislavovich@users.noreply.github.com>
2023-10-01 14:48:50 +0300

    Add files via upload

--- project/app_lesson_4/prilozh_app_lesson_4/apps.py
+++ project/app_lesson_4/prilozh_app_lesson_4/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PrilozhAppLesson4Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'prilozh_app_lesson_4'
+    verbose_name='Объявления'
--- project/app_lesson_4/prilozh_app_lesson_4/models.py
+++ project/app_lesson_4/prilozh_app_lesson_4/models.py
@@ -1,4 +1,8 @@
+from django.contrib import admin
 from django.db import models
+from django.utils import timezone
+from django.utils.html import format_html
+
 
 # Create your models here.
 class Advertisment(models.Model):
@@ -8,6 +12,23 @@ class Advertisment(models.Model):
     auction = models.BooleanField('Торг', help_text="отметьте, если торг уместен")
     created_at =models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date()==timezone.now().date():
+            created_time=self.created_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color:red;'
+                               'font-weight:bold"> Сегодня в'
+                               ' {}</span>',created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            created_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color:red;'
+                               'font-weight:bold"> Сегодня в'
+                               ' {}</span>', created_time)
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
 
     def __str__(self):
         return f'Advertisment(id={self.id},title={self.title},price={self.price})'
