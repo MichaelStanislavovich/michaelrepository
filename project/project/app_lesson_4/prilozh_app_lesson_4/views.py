e9175b9888df388d1c29088bc61d329e9f86b301
MichaelStanislavovich <143657967+MichaelStanislavovich@users.noreply.github.com>
2023-10-11 22:38:35 +0300

    Add files via upload

--- project/project/app_lesson_4/prilozh_app_lesson_4/views.py
+++ project/project/app_lesson_4/prilozh_app_lesson_4/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
+from .forms import AdvertisementForms
 # Create your views here.
 from .models import Advertisement
 
@@ -12,3 +14,16 @@ def index(request):
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
+def adv_post(request):
+    if request.method=='POST':
+        form = AdvertisementForms(request.POST, request.FILES)
+        if form.is_valid():
+            new_advertisement = form.save(commit=False)
+            new_advertisement.user = request.user
+            new_advertisement.save()
+            url = reverse('main_page')
+            return redirect(url)
+        else:
+            form = AdvertisementForms()
+        context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
